04bc1a0b4df26eb7af8c1edb9bbf094c5abd87f0
Hassan Razak <hassanrazak1991@gmail.com>
2022-11-08 11:05:38 -0500

    commit tree 8a65be12a57d48eb40f2b26aebc1a91054d80d07

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone","doll"];
+
+function wrapGifts(gift){
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+};
+
+wrapGifts(gifts);
+
+const messages = [];
+
+function writeCards(stringName,eventName){
+    for (let i = 0; i < stringName.length;i++){
+        
+        messages.push((`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!`));
+    
+    };
+
+return messages;
+
+};
+function countDown(integerPos){
+    let i = integerPos;
+    while(i >= 0){
+        console.log(i--);
+    }
+}
+
+
+countDown(10);
+
+
+
+
+
+
+
